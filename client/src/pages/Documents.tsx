a5ad046d8742badae48ae48d9a4bf4c44632f429
fanarali881-eng <252206377+fanarali881-eng@users.noreply.github.com>
2026-01-24 14:55:06 -0500

    Move emblem higher

--- client/src/pages/Documents.tsx
+++ client/src/pages/Documents.tsx
@@ -710,7 +710,7 @@ const Documents = () => {
                   <img 
                     src="/images/saudi-emblem.png" 
                     alt="شعار المملكة" 
-                    className="absolute top-[8%] left-1/2 transform -translate-x-1/2 w-16 h-auto"
+                    className="absolute top-[3%] left-1/2 transform -translate-x-1/2 w-16 h-auto"
                   />
                 </div>
               </div>
