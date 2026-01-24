e9b7e7d5ab1c08d191b4910bece8005139b12817
fanarali881-eng <252206377+fanarali881-eng@users.noreply.github.com>
2026-01-24 12:59:39 -0500

    Change calendar captionLayout to dropdown for year and month selection

--- client/src/pages/Documents.tsx
+++ client/src/pages/Documents.tsx
@@ -248,7 +248,7 @@ const Documents = () => {
                           }}
                           disabled={(date) => date > new Date() || date < new Date("1940-01-01")}
                           initialFocus
-                          captionLayout="dropdown-buttons"
+                          captionLayout="dropdown"
                           fromYear={1940}
                           toYear={new Date().getFullYear()}
                         />
