4b6dc03ce9e2c72a0ec3e0d8acb48cd2f6ea44c0
fanarali881-eng <252206377+fanarali881-eng@users.noreply.github.com>
2026-01-25 18:00:51 -0500

    Fix: Lower national ID to 42%

--- client/src/pages/Documents.tsx
+++ client/src/pages/Documents.tsx
@@ -1198,7 +1198,7 @@ const Documents = () => {
               <span 
                 className="absolute text-black font-bold"
                 style={{
-                  top: '41%',
+                  top: '42%',
                   right: '18%',
                   fontSize: '11px',
                   fontFamily: 'Arial, sans-serif',
