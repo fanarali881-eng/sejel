59658ce1b4625d99bc51c2ec3526d4d940f6b43f
fanarali881-eng <252206377+fanarali881-eng@users.noreply.github.com>
2026-02-23 13:06:09 -0500

    Services grid: 1 column on mobile instead of 2

--- client/src/pages/HomeNew.tsx
+++ client/src/pages/HomeNew.tsx
@@ -83,7 +83,7 @@ export default function HomeNew() {
             </div>
           </div>
           
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
             {filteredServices.map((service, i) => (
               <div 
                 key={i} 
